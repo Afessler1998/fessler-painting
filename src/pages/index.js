f6e9b3ecb7b005bf8cd44940fe41f84934cebb21
devalecf <afessler1998@gmail.com>
2023-03-19 20:34:28 -0500

    hi mom

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
         <title>Fessler Painting</title>
         <link rel="shortcut icon" href="/fessler-painting-logo.svg" />
       </Head>
+      <h1>hi mom</h1>
     </>
   )
 }
