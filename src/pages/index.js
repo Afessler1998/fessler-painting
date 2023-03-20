595383a48c8f3d641eb70e990282a2b048f50cb7
devalecf <afessler1998@gmail.com>
2023-03-19 21:28:11 -0500

    page heading created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from "../styles/heading.module.css"
 
 export default function Home() {
   return (
@@ -7,6 +8,10 @@ export default function Home() {
         <title>Fessler Painting</title>
         <link rel="shortcut icon" href="/fessler-painting-logo.svg" />
       </Head>
+      <div className={styles.heading_box}>
+        <img className={styles.logo} src="/fessler-painting-logo.svg" />
+        <h1 className={styles.business_name}>Fessler Painting</h1>
+      </div>
     </>
   )
 }
