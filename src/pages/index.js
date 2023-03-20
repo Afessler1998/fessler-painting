930e1e9fe063b34e7c3df4a480b2813e67389629
devalecf <afessler1998@gmail.com>
2023-03-19 20:27:29 -0500

    favicon for sure actually set this time

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Fessler Painting</title>
+        <link rel="shortcut icon" href="/fessler-painting-logo.svg" />
+      </Head>
     </>
   )
 }
