05bd567f5b41160377e3e54186cca83be6c60e24
devalecf <afessler1998@gmail.com>
2023-03-19 19:03:52 -0500

    favicon put in right spot (i think)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-<link rel="shortcut icon" href="/static/fessler-painting-logo.svg" />
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="shortcut icon" href="/static/fessler-painting-logo.svg" />
       <body>
         <Main />
         <NextScript />
