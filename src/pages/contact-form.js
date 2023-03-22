bca0d3adc00207655c3734bfbbdf395c4e7bd35d
devalecf <afessler1998@gmail.com>
2023-03-22 15:57:11 -0500

    did some contact form styling

--- src/pages/contact-form.js
+++ src/pages/contact-form.js
@@ -8,6 +8,7 @@ const contactForm = () => {
     const [firstName, setFirstName] = useState('');
     const [statusCode, setStatusCode] = useState(0);
     const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const nameRegex = /^[a-zA-Z]+$/;
     const phoneRegex = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/;
@@ -16,23 +17,29 @@ const contactForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
 
+        setLoading(true);
+
         if (!nameRegex.test(event.target.firstName.value) || !nameRegex.test(event.target.lastName.value)) {
             alert('Please enter a valid name');
+            setLoading(false);
             return;
         }
 
         if (!phoneRegex.test(event.target.phoneNumber.value)) {
             alert('Please enter a valid phone number');
+            setLoading(false);
             return;
         }
         
         if (!emailRegex.test(event.target.email.value)) {
             alert('Please enter a valid email');
+            setLoading(false);
             return;
         }
 
         if (!event.target.message.value) {
             alert('Please add a description of your project');
+            setLoading(false);
             return;
         }
     
@@ -60,14 +67,14 @@ const contactForm = () => {
         const responseData = await response.json();
 
         if (response.ok) {
-            if (statusCode !== 0) {
-                setStatusCode(0);
-                setMessage("");
-            }
+            setStatusCode(response.status);
+            setMessage("");
             setFirstName(data.firstName);
+            setLoading(false);
         } else {
             setStatusCode(response.status);
             setMessage(responseData.data);
+            setLoading(false);
         }
     }
 
@@ -75,26 +82,28 @@ const contactForm = () => {
         <>
             <Header />
             <form onSubmit={handleSubmit} className={styles.formContainer}>
-                <label htmlFor="firstName" className={styles.label}>First name:</label>
-                <input id="firstName" name="firstName" type="text" className={styles.input} placeholder="First name" />
+                <label htmlFor="firstName" className={styles.label}>Name *</label>
+                <label htmlFor="lastName" className={styles.label} />
+                <div className={styles.nameContainer}>
+                    <input id="firstName" name="firstName" type="text" className={styles.input} placeholder="First" />
+                    <input id="lastName" name="lastName" type="text" className={styles.input} placeholder="Last" />
+                </div>
 
-                <label htmlFor="lastName" className={styles.label}>Last name:</label>
-                <input id="lastName" name="lastName" type="text" className={styles.input} placeholder="Last name" />
-
-                <label htmlFor="phoneNumber" className={styles.label}>Phone number:</label>
+                <label htmlFor="phoneNumber" className={styles.label}>Phone number *</label>
                 <input id="phoneNumber" name="phoneNumber" type="text" className={styles.input} placeholder="Phone number" />
 
-                <label htmlFor="email" className={styles.label}>Email:</label>
+                <label htmlFor="email" className={styles.label}>Email *</label>
                 <input id="email" name="email" type="text" className={styles.input} placeholder="Email" />
 
-                <label htmlFor="message" className={styles.label}>Describe your project:</label>
-                <textarea id="message" name="message" type="text" className={styles.input} placeholder="Describe your project" />
+                <label htmlFor="message" className={styles.label}>Tell us about your project *</label>
+                <textarea id="message" name="message" type="text" className={styles.textbox} placeholder="Tell us about your project" />
 
-                <button type="submit" className={styles.submitBtn}>Submit</button>
+                {!loading && statusCode != 200 && <button type="submit" className={styles.submitBtn}>Submit</button>}
+                {loading && <div className={styles.loadingSpinner} />}
+                {statusCode === 400 && <p className={styles.errorMsg}>{message}</p>}
+                {statusCode === 500 && <p className={styles.errorMsg}>Something went wrong, please try again later</p>}
+                {firstName && <p className={styles.successMsg}>Your contact info has been received, thank you for reaching out {firstName}!</p>}
             </form>
-            {statusCode === 400 && <p className={styles.errorMsg}>{message}</p>}
-            {statusCode === 500 && <p className={styles.errorMsg}>Something went wrong, please try again later</p>}
-            {firstName && <p className={styles.successMsg}>Your contact info has been received, thank you for reaching out {firstName}!</p>}
             <Footer />
         </>
     );
